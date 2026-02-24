3d55bfc403faadadbdabe74f53b94bea3caca774
sejinlee123 <sejinlee123@berkeley.edu>
2026-02-24 04:01:27 -0800

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -12,10 +12,8 @@ resource "aws_apigatewayv2_stage" "default" {
 resource "aws_apigatewayv2_integration" "lambda_handler" {
   api_id             = aws_apigatewayv2_api.backend_api.id
   integration_type   = "AWS_PROXY"
-  # CHANGE THIS FROM "POST" TO "ANY"
-  integration_method = "ANY" 
+  integration_method = "POST"
   integration_uri    = aws_lambda_function.api.invoke_arn
-  payload_format_version = "2.0" # Ensure this is set for HTTP APIs
 }
 
 resource "aws_apigatewayv2_route" "proxy" {
