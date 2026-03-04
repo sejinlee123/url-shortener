f2de1d71089b1df5b5779ea6f3e507dc3f4041de
sejinlee123 <sejinlee123@berkeley.edu>
2026-03-03 18:23:20 -0800

    d

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,13 @@ provider "aws" {
   #profile = var.aws_profile
 }
 
+resource "null_resource" "invalidate_cache" {
+  triggers = {
+    # This triggers the invalidation whenever any file in your frontend changes
+    hash = md5(join("", [for f in fileset("${path.module}/dist", "**") : filemd5("${path.module}/dist/${f}")]))
+  }
+
+  provisioner "local-exec" {
+    command = "aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.website_cdn.id} --paths '/*'"
+  }
+}
