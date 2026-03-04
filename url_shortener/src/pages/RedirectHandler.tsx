065f1a2b986c3d407aa2a31f36f7cdeb6e0fd87b
sejinlee123 <sejinlee123@berkeley.edu>
2026-03-03 16:35:38 -0800

    Update RedirectHandler.tsx

--- url_shortener/src/pages/RedirectHandler.tsx
+++ url_shortener/src/pages/RedirectHandler.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {useParams, useNavigate} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {Loader2} from "lucide-react";
 
 export default function RedirectHandler() {
