e10425da12bfa6db3c44eab95cc93d8655ff96b8
sejinlee123 <sejinlee123@berkeley.edu>
2026-03-03 17:00:58 -0800

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -34,6 +34,13 @@
 
     custom_error_response {
       error_caching_min_ttl = 300
+      error_code            = 403
+      response_code         = "200"
+      response_page_path    = "/index.html"
+    }
+
+    custom_error_response {
+      error_caching_min_ttl = 300
       error_code            = 404
       response_code         = "200"
       response_page_path    = "/index.html"
